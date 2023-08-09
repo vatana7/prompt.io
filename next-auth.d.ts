261ff6731a0ab66172f037584efbd511c936d645
vatana.sambath <vatanamain@gmail.com>
2023-08-09 22:55:18 +0700

    bug: fix type error for usSession()

---
+++ next-auth.d.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session extends DefaultSession {
+    user?: {
+      /** The user's postal address. */
+      id?: string;
+      /** The user's name. */
+      name?: string;
+      /** The user's email address. */
+      email?: string;
+      /** The user's image icon. */
+      image?: string;
+    };
+    expires: string;
+  }
+}
